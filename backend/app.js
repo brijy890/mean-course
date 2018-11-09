84533f197331981bebc633a937a1d6b87bb44ec5
brijy890 <brijy123@gmail.com>
2018-11-09 22:23:52 +0530

    post api

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader(
-    "Access-Control-Allow-Header",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader("Access-Control-Allow-Methods",
@@ -14,7 +18,15 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
+  const posts = req.body;
+  console.log(posts);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
       id: 'fad123122',
