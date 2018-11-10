2f1ca46e40f567364994cf45e066b83ade8f01dd
brijy890 <brijy123@gmail.com>
2018-11-10 19:37:38 +0530

    spinner added

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -11,14 +11,17 @@ import { PostsService } from '../posts.service';
 })
 export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
+  isLoding = false;
   private postsSub = new Subscription;
 
   constructor(public postService: PostsService) { }
 
   ngOnInit() {
+    this.isLoding = true;
     this.postService.getPosts();
     this.postsSub = this.postService.getPostUpdateListner()
       .subscribe((posts: Post[]) => {
+        this.isLoding = false;
         this.posts = posts;
       });
   }
